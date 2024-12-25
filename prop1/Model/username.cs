38ba1f7001330fc43233eabd8ca2fac4e44f2f18
alene <alene@ABDALLAH>
2024-12-25 14:59:47 +0300

    Add project files.

---
+++ prop1/Model/username.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prop1.Model
+{
+    internal class username
+    {
+        private string usrName;
+        public string UName
+        {
+            get
+            {
+                return usrName;
+            }
+
+            set
+            {
+                if (value != null)
+                {
+                    usrName = value;
+                }
+                else Console.WriteLine("invalid username ");
+            }
+        }
+        private double salary;
+        public double Salary
+        {
+            get
+            {
+                return salary;
+            }
+
+            set
+            {
+                if (value >= 350 && value <= 900)
+                {
+                    salary = value;
+                }
+                else Console.WriteLine("out of range ");
+            }
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Username: {usrName} | Salary: {salary}");
+        }
+    }
+}
+
---
+++ prop1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using prop1.Model;
+string username;
+
+username D2 = new username();
+Console.WriteLine("enter username");
+D2.UName =Console.ReadLine();
+
+
+
+Console.WriteLine("enter salary");
+D2.Salary = Convert.ToInt32 (Console.ReadLine());
+
+D2.GetInfo();
